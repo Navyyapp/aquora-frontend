37de141c66ccd1b542190278f4a6cf5075ec9c5c
Navyyapp <ponugotinavya9542@gmail.com>
2025-11-04 20:30:38 +0530

    changes in frontend

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -87,7 +87,7 @@ export default function Cart({ cart, setCart }) {
 
     try {
       setLoading(true);
-      const response = await fetch("https://aquora-backend.vercel.app/api/orders", {
+      const response = await fetch("https://aquora-backend.vercel.app//api/orders", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(orderData),
